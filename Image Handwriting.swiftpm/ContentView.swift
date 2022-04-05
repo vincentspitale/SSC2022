f28df90df1f1df49b478afe8443325808cbcf2b1
Vincent Spitale <vincentgspitale@gmail.com>
2022-04-05 14:50:31 -0400

    first commit

---
+++ Image Handwriting.swiftpm/ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("With this app you can convert images of handwriting to vector paths. These paths can be manipulated exactly like those drawn directly on the device. For the best editing experience please use an Apple Pencil.")
+        }
+    }
+}
