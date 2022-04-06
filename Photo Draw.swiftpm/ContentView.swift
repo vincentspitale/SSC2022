3319826c1fe536c7a95bdfa2f6c06b18401f7a7d
Vincent Spitale <vincentgspitale@gmail.com>
2022-04-06 14:39:19 -0400

    app icons

--- Photo Draw.swiftpm/ContentView.swift
+++ Photo Draw.swiftpm/ContentView.swift
@@ -1,26 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isContinued: Bool = false
     var body: some View {
-        VStack {
-            Image("Icon")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: 200, maxHeight: 200)
-                .mask {
-                    RoundedRectangle(cornerRadius: 40,
-                                                             style: .continuous)
-                }
-                .padding()
-            
-            VStack(alignment: .leading) {
-                Text("Photo Draw").font(.largeTitle)
-                    .bold()
-                    .padding(.bottom)
-            Text("Photo Draw lets you convert images of handwriting to vector paths. These paths can then be manipulated exactly like those drawn directly on your device. For the best editing experience consider using an Apple Pencil.")
-                .frame(maxWidth: 600)
+        ZStack{
+            WelcomeView(isContinued: $isContinued)
+            if isContinued {
+                Rectangle()
+                    .foregroundColor(Color(uiColor: UIColor.systemBackground))
+                    .transition(.opacity)
             }
-        }.padding()
-        
+        }
     }
 }
---
+++ Photo Draw.swiftpm/WelcomeView.swift
@@ -0,0 +1,65 @@
+//
+//  WelcomeView.swift
+//  Photo Draw
+//
+//  Created by Vincent Spitale on 4/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct WelcomeView: View {
+    @Binding var isContinued: Bool
+    
+    var body: some View {
+        ZStack{
+        ScrollView{
+            HStack{
+                Spacer()
+            }
+        VStack {
+            Spacer()
+            Image("Icon")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 200, maxHeight: 200)
+                .mask {
+                    RoundedRectangle(cornerRadius: 40,
+                                                             style: .continuous)
+                }
+                .padding()
+                .padding(.vertical, 40)
+            
+            VStack(alignment: .leading) {
+                Text("Photo Draw").font(.largeTitle)
+                    .bold()
+                    .padding(.bottom)
+            Text("Photo Draw lets you convert images with handwriting to vector paths. These paths can then be manipulated exactly like they were drawn directly on your device. For the best editing experience consider using an Apple Pencil.")
+            }
+            .lineSpacing(10)
+            .frame(maxWidth: 600)
+            .padding()
+            Spacer().frame(height: 100)
+            Spacer()
+        }
+        }
+            VStack{
+                Spacer()
+                Button(action: {
+                    withAnimation{$isContinued.wrappedValue.toggle()}}) {
+                Text("Continue")
+                    .font(.headline)
+                    .bold()
+                    .foregroundColor(Color(uiColor: UIColor.systemBackground))
+                    .padding()
+                    .frame(width: 200)
+                .background(Color.accentColor)
+                .cornerRadius(15)
+            }
+            .padding(.bottom, 40)
+            .padding(.horizontal)
+            }
+            
+        }
+    }
+}
